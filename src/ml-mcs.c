15733aa3f7d4e4da1de850894287652b9d55dd74
po-ju.chen <tonyone0902@gmail.com>
2016-08-22 16:19:09 +0800

    upgrade to new jerry api

--- src/ml-mcs.c
+++ src/ml-mcs.c
@@ -1,5 +1,5 @@
 #include <string.h>
-#include "jerry.h"
+#include "jerry-api.h"
 #include "microlattice.h"
 #include "MQTTClient.h"
 #include "httpclient.h"
@@ -40,70 +40,44 @@ char *mcs_replace(char *st, char *orig, char *repl)
   return buffer;
 }
 
-void livereload(char *url)
-{
-  jerry_cleanup();
+// void livereload(char *url)
+// {
+//   jerry_cleanup();
 
-  httpclient_t client = {0};
-  httpclient_data_t client_data = {0};
-  char *buf;
+//   httpclient_t client = {0};
+//   httpclient_data_t client_data = {0};
+//   char *buf;
 
-  buf = pvPortMalloc(BUF_SIZE);
-  if (buf == NULL) {
-      printf("httpclient_test malloc failed.\r\n");
-      return;
-  }
-  client_data.response_buf = buf;
-  client_data.response_buf_len = BUF_SIZE;
+//   buf = pvPortMalloc(BUF_SIZE);
+//   if (buf == NULL) {
+//       printf("httpclient_test malloc failed.\r\n");
+//       return;
+//   }
+//   client_data.response_buf = buf;
+//   client_data.response_buf_len = BUF_SIZE;
 
-  httpclient_get(&client, url, HTTP_PORT, &client_data);
-  printf("url: %s\n", url);
-  strcpy(&script, client_data.response_buf);
-  _js_init();
-  vPortFree(buf);
-}
+//   httpclient_get(&client, url, HTTP_PORT, &client_data);
+//   printf("url: %s\n", url);
+//   strcpy(&script, client_data.response_buf);
+//   _js_init();
+//   vPortFree(buf);
+// }
 
-void _js_init()
-{
-  jerry_init (JERRY_FLAG_EMPTY);
-  jerry_api_value_t eval_ret;
-  js_lib_init("init");
-  jerry_api_eval (&script, strlen (&script), false, false, &eval_ret);
-  jerry_api_release_value (&eval_ret);
+// void _js_init()
+// {
+//   jerry_init (JERRY_FLAG_EMPTY);
+//   jerry_value_t eval_ret;
+//   js_lib_init("init");
+//   jerry_api_eval (&script, strlen (&script), false, false, &eval_ret);
+//   jerry_api_release_value (&eval_ret);
 
-  vTaskDelete(NULL);
-}
+//   vTaskDelete(NULL);
+// }
 
 DELCARE_HANDLER(__mcs)
 {
   arrivedcount = 0;
-  /* server */
-  int server_req_sz = -jerry_api_string_to_char_buffer (args_p[0].v_string, NULL, 0);
-  char * server_buffer = (char*) malloc (server_req_sz);
-  server_req_sz = jerry_api_string_to_char_buffer (args_p[0].v_string, server_buffer, server_req_sz);
-  server_buffer[server_req_sz] = '\0';
 
-  /* port */
-  int port_req_sz = -jerry_api_string_to_char_buffer (args_p[1].v_string, NULL, 0);
-  char * port_buffer = (char*) malloc (port_req_sz);
-  port_req_sz = jerry_api_string_to_char_buffer (args_p[1].v_string, port_buffer, port_req_sz);
-  port_buffer[port_req_sz] = '\0';
-
-  /* topic */
-  int topic_req_sz = -jerry_api_string_to_char_buffer (args_p[2].v_string, NULL, 0);
-  char * topic_buffer = (char*) malloc (topic_req_sz);
-  topic_req_sz = jerry_api_string_to_char_buffer (args_p[2].v_string, topic_buffer, topic_req_sz);
-  topic_buffer[topic_req_sz] = '\0';
-
-  /* clientId */
-  int clientId_req_sz = -jerry_api_string_to_char_buffer (args_p[3].v_string, NULL, 0);
-  char * clientId_buffer = (char*) malloc (clientId_req_sz);
-  clientId_req_sz = jerry_api_string_to_char_buffer (args_p[3].v_string, clientId_buffer, clientId_req_sz);
-  clientId_buffer[clientId_req_sz] = '\0';
-
-  /* tls */
-
-  printf("topic: %s\n", topic_buffer);
   unsigned char msg_buf[200];     //generate messages such as unsubscrube
   unsigned char msg_readbuf[200]; //receive messages such as unsubscrube ack
 
@@ -112,6 +86,18 @@ DELCARE_HANDLER(__mcs)
 
   //init mqtt network structure
   NewNetwork(&n);
+
+  /* server */
+  jerry_size_t server_req_sz = jerry_get_string_size (args_p[0]);
+  jerry_char_t server_buffer[server_req_sz];
+  jerry_string_to_char_buffer (args_p[0], server_buffer, server_req_sz);
+  server_buffer[server_req_sz] = '\0';
+
+  /* port */
+  jerry_size_t port_req_sz = jerry_get_string_size (args_p[1]);
+  jerry_char_t port_buffer[port_req_sz];
+  jerry_string_to_char_buffer (args_p[1], port_buffer, port_req_sz);
+  port_buffer[port_req_sz] = '\0';
 
   rc = ConnectNetwork(&n, server_buffer, port_buffer);
 
@@ -122,6 +108,11 @@ DELCARE_HANDLER(__mcs)
 
   //init mqtt client structure
   MQTTClient(&c, &n, 12000, msg_buf, 200, msg_readbuf, 200);
+
+  jerry_size_t clientId_req_sz = jerry_get_string_size (args_p[3]);
+  jerry_char_t clientId_buffer[clientId_req_sz];
+  jerry_string_to_char_buffer (args_p[3], clientId_buffer, clientId_req_sz);
+  clientId_buffer[clientId_req_sz] = '\0';
 
   //mqtt connect req packet header
   data.willFlag = 0;
@@ -139,18 +130,21 @@ DELCARE_HANDLER(__mcs)
     printf("MQTT connect fail,status%d\n", rc);
   }
 
-  printf("Subscribing to %s\n", topic_buffer);
+  // printf("Subscribing to %s\n", topic_buffer);
 
   void messageArrived(MessageData *md) {
     // char rcv_buf_old[100] = {0};
     MQTTMessage *message = md->message;
 
-    jerry_api_value_t params[0];
-    params[0].type = JERRY_API_DATA_TYPE_STRING;
-    params[0].v_string = jerry_api_create_string (message->payload);
+    jerry_value_t params[0];
+    params[0] = jerry_create_string(message->payload);
 
-    jerry_api_call_function(args_p[5].v_object, NULL, false, &params, 1);
-    jerry_api_release_value(&params);
+    jerry_value_t this_val = jerry_create_undefined();
+    jerry_value_t ret_val = jerry_call_function (args_p[5], this_val, &params, 1);
+
+    jerry_release_value(params);
+    jerry_release_value(this_val);
+    jerry_release_value(ret_val);
 
     strcpy(rcv_buf, message->payload);
     strcpy(split_buf, rcv_buf);
@@ -162,17 +156,21 @@ DELCARE_HANDLER(__mcs)
     if (0 == strncmp (arr[1], "FOTA", 4)) {
       char *s = mcs_replace(arr[4], "https", "http");
       fota_download_by_http(s);
-    } else if ( 0 == strncmp (arr[1], "Livereload", 10)){
-      printf("livereloadUrl: %s\n", arr[2]);
-      livereload(arr[2]);
     }
-
+    // } else if ( 0 == strncmp (arr[1], "Livereload", 10)){
+    //   printf("livereloadUrl: %s\n", arr[2]);
+    //   livereload(arr[2]);
+    // }
     memset(rcv_buf, 0, 100);
     memset(split_buf, 0, MAX_STRING_SIZE);
-
   }
 
-  switch ((int)args_p[4].v_float32) {
+  jerry_size_t topic_req_sz = jerry_get_string_size (args_p[2]);
+  jerry_char_t topic_buffer[topic_req_sz];
+  jerry_string_to_char_buffer (args_p[2], topic_buffer, topic_req_sz);
+  topic_buffer[topic_req_sz] = '\0';
+
+  switch ((int) jerry_get_number_value(args_p[4])) {
     case 0:
       rc = MQTTSubscribe(&c, topic_buffer, QOS0, messageArrived);
       break;
@@ -188,31 +186,30 @@ DELCARE_HANDLER(__mcs)
     MQTTYield(&c, 1000);
   }
 
-  ret_val_p->type = JERRY_API_DATA_TYPE_BOOLEAN;
-  ret_val_p->v_bool = true;
   free(server_buffer);
   free(topic_buffer);
   free(port_buffer);
   free(clientId_buffer);
-  return true;
+
+  return jerry_create_boolean(true);
 }
 
 DELCARE_HANDLER(__mcsClose)
 {
   arrivedcount = 1;
-  return true;
+  return jerry_create_boolean(true);
 }
 
 DELCARE_HANDLER(__mcsSend)
 {
   char buf[100];
 
-  int msg_req_sz = -jerry_api_string_to_char_buffer (args_p[0].v_string, NULL, 0);
-  char * msg_buffer = (char*) malloc (msg_req_sz);
-  msg_req_sz = jerry_api_string_to_char_buffer (args_p[0].v_string, msg_buffer, msg_req_sz);
+  jerry_size_t msg_req_sz = jerry_get_string_size (args_p[0]);
+  jerry_char_t msg_buffer[msg_req_sz];
+  jerry_string_to_char_buffer (args_p[0], msg_buffer, msg_req_sz);
   msg_buffer[msg_req_sz] = '\0';
 
-  switch ((int)args_p[1].v_float32) {
+  switch ((int) jerry_get_number_value(args_p[1])) {
     case 0:
       message.qos = QOS0;
       break;
@@ -229,46 +226,47 @@ DELCARE_HANDLER(__mcsSend)
   message.payload = (void *)msg_buffer;
   message.payloadlen = strlen(msg_buffer) + 1;
   rc = MQTTPublish(&c, topic_buffer, &message);
-  return true;
+
+  return jerry_create_boolean(true);
 }
 
-DELCARE_HANDLER(__rebootScript)
-{
-  jerry_cleanup();
+// DELCARE_HANDLER(__rebootScript)
+// {
+//   jerry_cleanup();
 
-  /* url */
-  int url_req_sz = -jerry_api_string_to_char_buffer (args_p[0].v_string, NULL, 0);
-  char * url_buffer = (char*) malloc (url_req_sz);
-  url_req_sz = jerry_api_string_to_char_buffer (args_p[0].v_string, url_buffer, url_req_sz);
-  url_buffer[url_req_sz] = '\0';
+//   /* url */
+//   int url_req_sz = -jerry_api_string_to_char_buffer (args_p[0].v_string, NULL, 0);
+//   char * url_buffer = (char*) malloc (url_req_sz);
+//   url_req_sz = jerry_api_string_to_char_buffer (args_p[0].v_string, url_buffer, url_req_sz);
+//   url_buffer[url_req_sz] = '\0';
 
-  httpclient_t client = {0};
-  httpclient_data_t client_data = {0};
-  char *buf;
+//   httpclient_t client = {0};
+//   httpclient_data_t client_data = {0};
+//   char *buf;
 
-  buf = pvPortMalloc(BUF_SIZE);
-  if (buf == NULL) {
-      printf("httpclient_test malloc failed.\r\n");
-      return;
-  }
-  client_data.response_buf = buf;
-  client_data.response_buf_len = BUF_SIZE;
+//   buf = pvPortMalloc(BUF_SIZE);
+//   if (buf == NULL) {
+//       printf("httpclient_test malloc failed.\r\n");
+//       return;
+//   }
+//   client_data.response_buf = buf;
+//   client_data.response_buf_len = BUF_SIZE;
 
-  httpclient_get(&client, url_buffer, HTTP_PORT, &client_data);
+//   httpclient_get(&client, url_buffer, HTTP_PORT, &client_data);
 
-  strcpy(&script, client_data.response_buf);
-  _js_init();
-  vPortFree(buf);
+//   strcpy(&script, client_data.response_buf);
+//   _js_init();
+//   vPortFree(buf);
 
-  ret_val_p->type = JERRY_API_DATA_TYPE_BOOLEAN;
-  ret_val_p->v_bool = true;
-  return true;
-}
+//   // ret_val_p->type = JERRY_API_DATA_TYPE_BOOLEAN;
+//   // ret_val_p->v_bool = true;
+//   return jerry_create_boolean(true);
+// }
 
 void ml_mcs_init(void)
 {
   REGISTER_HANDLER(__mcs);
   REGISTER_HANDLER(__mcsSend);
   REGISTER_HANDLER(__mcsClose);
-  REGISTER_HANDLER(__rebootScript);
+  // REGISTER_HANDLER(__rebootScript);
 }
